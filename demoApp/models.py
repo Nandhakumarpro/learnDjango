0deff43b016fda13628985ddd3b1ab8595ecbde5
Nandhakumarpro <>
2021-02-19 10:42:58 +0530

    post model commit

--- demoApp/models.py
+++ demoApp/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20, null=False, unique=True )
     password = models.CharField(max_length=20,)#max_length=200, null= True )
+
+class Post(models.Model):
+    title = models.CharField(max_length=20,null=False)
+    desc = models.CharField(max_length=1000, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
